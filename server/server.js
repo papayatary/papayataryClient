4737a105940cda77c8c2ff9c4c9bdfd5eda5bb38
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-04-28 18:58:36 -0700

    Updated server to seperate config and routes into seperate concerns

--- server/server.js
+++ server/server.js
@@ -1,20 +1,20 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
 var app = express();
-var mainRouter = require('./routes/mainRouter.js');
-//define process variables
 var port = process.env.PORT || 8000;
-//configuration
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../client'));
-//routing
-app.use(mainRouter);
 
+//config app with required middleware
+require('./config/middleware.js')(app, express);
+
+//define routes for app
+require('./routes/routes.js')(app, express);
+
+// start listening to requests on port 8000
 app.listen(port, function(err){
   if(err) {
     return console.log('error listening on port'+port, err);
   }
   console.log('App is listening on port '+port);
 });
+
+// export our app for testing and flexibility
+module.exports = app;
