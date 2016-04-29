76e7dbcf032458b1a6febc3f52b6af4f0abf8499
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-04-28 19:19:56 -0700

    Remove hello world route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,11 @@
 var express = require('express');
-// custom helper middleware
+var path = require('path');
+// custom helper middleware 
 var helpers = require('../config/helpers.js');
 
-module.exports = function (app, express) {
-  // app.get('/', function(err, res){
-  //   res.json('Hello World');
-  // });
-  //app.get('/example', examplecontroller);
+module.exports =  (app, express) => {
+  
+  // app.get('/example', examplecontroller);
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
