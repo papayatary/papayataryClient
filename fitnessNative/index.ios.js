a4d60e71807e89d4db47d06755648e837890af7f
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-04-29 17:29:06 -0700

    Implement Facebook Login

--- fitnessNative/index.ios.js
+++ fitnessNative/index.ios.js
@@ -12,20 +12,16 @@ import React, {
   View
 } from 'react-native';
 
+import FBLogin = require('react-native-facebook-login');
+
 class fitnessNative extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View>
+      <Text style={styles.welcome} numberOfLines={2}>
+                supg
+              </Text>
+              <FBLogin/>
       </View>
     );
   }
