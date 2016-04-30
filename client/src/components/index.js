d68cda895c6f64003d0ba05d824163f70a83f5e3
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-04-30 16:50:48 -0700

    Add components skeleton

---
+++ client/src/components/index.js
@@ -0,0 +1,31 @@
+// EXAMPLE CODE:
+// This is the main entry point for the app. It renders the App component after wrapping it in the Provider from Redux.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import rootReducer from '../reducers';
+import { Provider } from 'react-redux'
+
+import App from './App.js'
+
+// Generate initial state
+let initialState = {
+  todos: [{
+    id: 0,
+    completed: false,
+    text: 'Initial todo for demo purposes'
+  }]
+}
+
+// Create Redux store with initial state. The store manages the state of our app. 
+let store = createStore(rootReducer, initialState);
+
+var render = function() {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('app')
+  );
+};
+render();
