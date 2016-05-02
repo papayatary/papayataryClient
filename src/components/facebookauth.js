27f760d61662a1aa844e546079de7013861fbc91
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-02 16:11:28 -0700

    Add files to reorganize repo

---
+++ src/components/facebookauth.js
@@ -0,0 +1,48 @@
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import FitbitAuth from './fitbitauth';
+
+
+class FacebookAuth extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          This is the FacebookAuth view!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default FacebookAuth;
---
+++ src/reducers/user.js
@@ -0,0 +1,14 @@
+
+// EXAMPLE CODE:
+const initialState = {
+  email: null,
+  firstName: null,
+  lastName: null
+};
+
+export default function user (state = initialState, action) {
+  switch (action.type) {
+  default:
+    return state;
+  }
+}
