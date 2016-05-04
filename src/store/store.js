5167e14280eda578649bf6e24d81019237b23792
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-04 16:42:09 -0700

    Add remoteDev middleware to store

--- src/store/store.js
+++ src/store/store.js
@@ -5,13 +5,16 @@ import {
   createStore } from 'redux'
 import rootReducer from '../reducers/root'
 import logger from 'redux-logger'
+import devTools from 'remote-redux-devtools';
 
 //this is function that lets us add a "logger" middleware to the original createStore 
 let finalCreateStore = compose(
-  applyMiddleware(logger())
-)(createStore)
+  applyMiddleware(logger()),
+  devTools()
+)(createStore);
 
 //good to separate out store.js because you can add middleware (above)
 export default function configureStore(initialState) {
+
   return finalCreateStore(rootReducer, initialState)
 }
