bf513a33e6542817e03c6dd1b1f066e74f661bab
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-05-03 17:24:43 -0700

    (feat) Complete save facebook credentials functionality on successful user login

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -7,15 +7,17 @@
 //   age: null
 // };
 const initialState = {
-
 };
 
 export default function user (state = initialState, action) {
   switch (action.type) {
     case 'SAVE_FACEBOOK_CREDENTIALS':
-      console.log('State: ', state);
+      console.log('Old State: ', state);
       console.log('Action: ', action);
-      return state;
+      var newState = Object.assign({}, state);
+      newState.facebookCredentials = action.credentials.credentials;
+      console.log('New State: ', newState.facebookCredentials);
+      return newState;
     default:
       return state;
   }
