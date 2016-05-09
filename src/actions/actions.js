82c1d8f2e8be00a838a02e7735e8141925580bbb
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-05-09 15:29:28 -0700

    (feat) Cleanup zombie code

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -17,26 +17,3 @@ module.exports = {
     return { type: 'SET_CURRENT_MESSAGE_ID', facebookId };
   }
 };
-
-//---------------- EXAMPLE CODE: ---------------//
-// module.exports = {
-//   updatePlaces: function(places) {
-//     return { type: 'UPDATE_PLACES', places };
-//   },
-
-//   savePlace: function(place) {
-//     return { type: 'SAVE_PLACE', place };
-//   },
-
-//   deletePlace: function(placeId) {
-//     return { type: 'DELETE_PLACE', placeId };
-//   },
-
-//   saveCoordinate: function(coord) {
-//     return { type: 'SAVE_COORDINATE', coord};
-//   },
-
-//   fetchCoordinate: function() {
-//     return { type: 'FETCH_COORDINATE' };
-//   }
-// };
