772151bd60cd18553eb5369a568ef7e607557aa2
Kenneth Montgomery <kennethMontgo@gmail.com>
2016-05-08 21:19:57 -0700

    Android files updated automatically

--- android/app/src/main/java/com/client/MainActivity.java
+++ android/app/src/main/java/com/client/MainActivity.java
@@ -1,6 +1,7 @@
 package com.client;
 
 import com.facebook.react.ReactActivity;
+import com.magus.fblogin.FacebookLoginPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -36,6 +37,7 @@ public class MainActivity extends ReactActivity {
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
             new MainReactPackage(),
+            new FacebookLoginPackage(),
             new VectorIconsPackage()
         );
     }
