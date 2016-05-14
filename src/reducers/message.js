3680fd8e4cd2795d837c0d1221fe98acd38874b1
haomingh <haomingh@users.noreply.github.com>
2016-05-13 19:32:55 -0700

    (feat) Add facebook id to message store

--- src/reducers/message.js
+++ src/reducers/message.js
@@ -8,6 +8,7 @@
 // };
 const initialState = {
   toUserId: 2,
+  facebookId: 'b1020642324234696',
   firstName: 'Blake',
   lastName: 'Lively',
   picturePath: 'http://hbz.h-cdn.co/assets/cm/14/52/54988f0f50262_-_hbz-blake-lively-style-alert-promo-xln.jpg',
@@ -18,6 +19,7 @@ export default function message (state = initialState, action) {
     case 'SET_CURRENT_MESSAGE_ID':
       var newState = {};
       newState.toUserId = action.toUserData.toUserId;
+      newState.facebookId = action.toUserData.facebookId;
       newState.firstName = action.toUserData.firstName;
       newState.lastName = action.toUserData.lastName;
       newState.picturePath = 'http://hbz.h-cdn.co/assets/cm/14/52/54988f0f50262_-_hbz-blake-lively-style-alert-promo-xln.jpg';
